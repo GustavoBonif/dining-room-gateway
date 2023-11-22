27224c3f5141aff6e2d105275b25fce628c66796
GustavoBonif <gubonic@gmail.com>
2023-11-22 11:36:20 -0300

    setup: creating api gateway service

---
+++ src/main/java/com/diningroom/gateway/DiningRoomGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.diningroom.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningRoomGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiningRoomGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diningroom/gateway/DiningRoomGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diningroom.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningRoomGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
